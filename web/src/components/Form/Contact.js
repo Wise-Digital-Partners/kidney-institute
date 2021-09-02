a2ce67e9c7e5462b73e49e008bd7a5cc68f70b40
Hamlet <55106549+hamletavetisian@users.noreply.github.com>
2021-09-02 08:34:45 -0700

    Update Contact.js

--- web/src/components/Form/Contact.js
+++ web/src/components/Form/Contact.js
@@ -36,6 +36,23 @@ export default class Form extends Component {
     this.state = { isValidated: false };
   }
 
+  changeFormName(event) {
+    // The value of the subject field.
+    var value = event.target.value;
+    // The name we want to apply to the form, based on the value.
+    var name = "Contact-" + value;
+    // The form element in the DOM.
+    var form = document.querySelector("#conditional-form");
+    // Apply the new name to the form's [name] attribute.
+    if (form) form.setAttribute("name", name);
+    // The [name="form-name] field in the DOM.
+    var formName = document.querySelector(
+      '#conditional-form [name="form-name"]'
+    );
+    // Apply the new name to the [name="form-name"] field within the form.
+    if (formName) formName.setAttribute("value", name);
+  }
+
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
@@ -69,15 +86,20 @@ export default class Form extends Component {
       <StyledForm>
         <div id="contact-form-ajax-response"></div>
         <form
-          name="Contact"
+          name="Contact-Dialysis Clinic"
           method="post"
           action=""
           data-netlify="true"
           data-netlify-honeypot="bot-field"
           onSubmit={this.handleSubmit}
+          id="conditional-form"
         >
           {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="Contact" />
+          <input
+            type="hidden"
+            name="form-name"
+            value="Contact-Dialysis Clinic"
+          />
           <div hidden>
             <label>
               Don’t fill this out:{" "}
@@ -145,7 +167,8 @@ export default class Form extends Component {
                     type="radio"
                     name="looking-for"
                     value="Dialysis Clinic"
-                    onChange={this.handleChange}
+                    // onChange={this.handleChange}
+                    onChange={this.changeFormName}
                     required={true}
                   />
                   <i className="far fa-check"></i>
@@ -159,7 +182,8 @@ export default class Form extends Component {
                     type="radio"
                     name="looking-for"
                     value="Nephrology Office"
-                    onChange={this.handleChange}
+                    onChange={this.changeFormName}
+                    // onChange={this.handleChange}
                   />
                   <i className="far fa-check"></i>
                   <span className="radio-custom text-sm md:text-base ">
@@ -187,6 +211,112 @@ export default class Form extends Component {
           </div>
 
           <ButtonSolid type="submit" text="Send Message" />
+        </form>
+
+        <form
+          name="Contact-Nephrology Office"
+          method="post"
+          action=""
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          // onSubmit={this.handleSubmit}
+          className="hidden"
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input
+            type="hidden"
+            name="form-name"
+            value="Contact-Nephrology Office"
+          />
+          <div hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" />
+            </label>
+          </div>
+
+          {/* <div className="mb-5"> */}
+          {/* <label
+              className="font-body text-sm font-semibold text-secondary-900 block mb-1"
+              htmlFor="name"
+            >
+              Name
+            </label> */}
+          <input type="text" name="name" />
+          {/* </div> */}
+
+          {/* <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-6 w-full"> */}
+          {/* <div className="mb-5"> */}
+          {/* <label
+                className="font-body text-sm font-semibold text-secondary-900 block mb-1"
+                htmlFor="phone"
+              >
+                Phone Number
+              </label> */}
+          <input type="tel" name="phone" />
+          {/* </div> */}
+          {/* <div className="mb-5"> */}
+          {/* <label
+                className="font-body text-sm font-semibold text-secondary-900 block mb-1"
+                htmlFor="email"
+              >
+                Email Address
+              </label> */}
+          <input type="email" name="email" />
+          {/* </div> */}
+          {/* </div> */}
+
+          {/* <div className="mb-5"> */}
+          {/* <fieldset> */}
+          {/* <legend
+                className="font-body text-sm font-semibold text-secondary-900 block mb-1"
+                htmlFor="looking-for"
+              >
+                What are you looking for?
+              </legend> */}
+          {/* <div className="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-8 mt-1 mb-1.5"> */}
+          {/* <label className="relative cursor-pointer"> */}
+          <input
+            type="radio"
+            name="looking-for"
+            value="Dialysis Clinic"
+            // onChange={this.handleChange}
+          />
+          {/* <i className="far fa-check"></i>
+                  <span className="radio-custom text-sm md:text-base">
+                    Dialysis Clinic
+                  </span> */}
+          {/* </label> */}
+
+          {/* <label className="relative cursor-pointer"> */}
+          <input
+            type="radio"
+            name="looking-for"
+            value="Nephrology Office"
+            // onChange={this.handleChange}
+          />
+          {/* <i className="far fa-check"></i>
+                  <span className="radio-custom text-sm md:text-base ">
+                    Nephrology Office
+                  </span> */}
+          {/* </label> */}
+          {/* </div> */}
+          {/* </fieldset> */}
+          {/* </div> */}
+
+          {/* <div className="mb-5"> */}
+          {/* <label
+              className="font-body text-sm font-semibold text-secondary-900 block mb-1"
+              htmlFor="message"
+            >
+              Message (optional)
+            </label> */}
+          <textarea
+            className="textarea"
+            name="message"
+            // onChange={this.handleChange}
+            rows="4"
+          />
+          {/* </div> */}
         </form>
       </StyledForm>
     );
