a41c63c00fa9fb5df64f4b65669af214392ba182
Hamlet <55106549+hamletavetisian@users.noreply.github.com>
2021-09-08 10:54:03 -0700

    Updates

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -64,16 +64,3 @@ exports.onCreateWebpackConfig = ({ stage, actions, loaders }) => {
     });
   }
 };
-
-const replacePath = (path) => (path === `/` ? path : path.replace(/\/$/, ``));
-
-exports.onCreatePage = ({ page, actions }) => {
-  const { createRedirect } = actions;
-  if (!page.path.includes(".html") && page.path !== "/") {
-    createRedirect({
-      fromPath: `${page.path}/`,
-      toPath: page.path,
-      isPermanent: true,
-    });
-  }
-};
